e6d2f7f549cf9c2bd1a615591b8d538762b15760
null <null>
2018-03-30 22:51:10 +0300

    null

--- src/firefox/user.js
+++ src/firefox/user.js
@@ -5,7 +5,6 @@ user_pref("dom.disable_beforeunload", true); //Отключить предупр
 user_pref("intl.locale.matchOS", false); //Использовать локаль из general.useragent.locale, а не установленную в ОС
 user_pref("browser.link.open_newwindow.restriction", 0); //Открывать попапы во вкладках вместо отдельных окон
 user_pref("browser.tabs.closeWindowWithLastTab", false); //Не закрывать Firefox при закрытии последней вкладки
-user_pref("browser.tabs.animate", false); // Отключает притормаживающую на окнах с многими вкладками анимацию открытия и закрытия табов.
 user_pref("dom.webnotifications.enabled", false); //отключаем надоедливые окошки с предложением включить уведомления
 user_pref("dom.webnotifications.serviceworker.enabled", false); //без этого не работает
 user_pref("browser.xul.error_pages.expert_bad_cert", true); // Более информативная страница показа ошибок в сертификатах сайтов.
@@ -19,3 +18,5 @@ user_pref("browser.urlbar.decodeURLsOnCopy", true);
 //хотя по идеи она и так должна быть отключена при отключении browser.safebrowsing через стандартные настройки
 user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+user_pref("network.proxy.socks_remote_dns", true); // dns через прокси
